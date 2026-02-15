deb8986d1ba46c98396d06a883a36fab26cf87fd
Elena <zanthia.lt@gmail.com>
2026-02-15 22:04:46 +0200

    First tool using HTTP

---
+++ mcpulsor-server/src/main/java/com/elsh/mcpulsor/server/McpulsorServerApplication.java
@@ -0,0 +1,66 @@
+package com.elsh.mcpulsor.server;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.json.jackson.JacksonMcpJsonMapper;
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.HttpServletStreamableServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema;
+import lombok.SneakyThrows;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class McpulsorServerApplication {
+    @SneakyThrows
+    public static void main(String[] args) {
+        System.out.println("Server Application Started");
+
+        HttpServletStreamableServerTransportProvider transportProvider = HttpServletStreamableServerTransportProvider.builder()
+                .mcpEndpoint("/mcpulsor")
+                .build();
+
+        McpSchema.Tool bioSensorTool = McpSchema.Tool.builder()
+                .name("bioSensor")
+                .title("Human Vital Pulse Sensor")
+                .description("Returns the current heart rate of the user as a simple string value")
+                .inputSchema(new JacksonMcpJsonMapper(new ObjectMapper()), createBioSensorSchema())
+                .build();
+
+        McpServerFeatures.SyncToolSpecification bioSensorToolSpec = McpServerFeatures.SyncToolSpecification.builder()
+                .tool(bioSensorTool)
+                .callHandler((mcpSyncServerExchange, callToolRequest) ->
+                        new McpSchema.CallToolResult("пульс пользователя 42", false))
+                .build();
+
+        McpSyncServer mcpSyncServer = McpServer.sync(transportProvider)
+                .serverInfo("mcpulsor mcp server", "1.0.RELEASE")
+                .capabilities(createServerCapabilities())
+                .tools(bioSensorToolSpec)
+                .build();
+
+        Server server = new Server(8091);
+
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.setContextPath("/");
+        contextHandler.addServlet(new ServletHolder(transportProvider), "/*");
+
+        server.setHandler(contextHandler);
+
+        server.start();
+        server.join();
+    }
+
+    private static String createBioSensorSchema() {
+        return new ObjectMapper().createObjectNode()
+                .put("type", "object")
+                .toString();
+    }
+
+    private static McpSchema.ServerCapabilities createServerCapabilities() {
+        return McpSchema.ServerCapabilities.builder()
+                .tools(true)
+                .build();
+    }
+}
